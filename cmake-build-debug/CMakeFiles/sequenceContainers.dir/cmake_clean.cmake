96ee85386f8ba17d911688861b0e858649cf675b
guidoasbun <guido@asbun.io>
2021-10-03 00:05:41 -0700

    Initial main

---
+++ cmake-build-debug/CMakeFiles/sequenceContainers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sequenceContainers.dir/main.cpp.o"
+  "sequenceContainers"
+  "sequenceContainers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sequenceContainers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
